1c38b9644322862f4b372af30ce5ef00efb6faba
Adel Saad <1code.abc@gmail.com>
2025-09-16 19:03:54 -0400

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+//  Render backend URL
+const API_URL = 'https://employee-manager-lqdr.onrender.com/api';
 
 // Get all employees
 export const getEmployees = () => axios.get(`${API_URL}/employees`);
